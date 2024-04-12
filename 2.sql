759e680b64ac3f9c0a87cf5d9ce388914c8a6427
VVV <VasilenkoVadim@mail.ru>
2024-04-12 11:30:09 +0300

    Аттестация создание

--- 2.sql
+++
@@ -1,9 +0,0 @@
-CREATE FUNCTION GetEvenNumbers()
-RETURNS TABLE
-AS
-RETURN
-(
-  SELECT number
-  FROM (VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10)) AS numbers(number)
-  WHERE number % 2 = 0
-);
